e0c1adf8245d71475938b2d64d58850dafa8b0e6
Austin Lazarus <austin.lazarus@gmail.com>
2017-01-13 10:38:08 -0400

    commit - 13.01.2016

--- app/api/wrappers/post.py
+++ app/api/wrappers/post.py
@@ -1,7 +1,6 @@
 from flask import session
 from app import db
 from app.models import Api
-import pickle 
 
 #************************
 #=> POST manager
@@ -12,34 +11,17 @@ class PostManager(object):
 
     @staticmethod
     def post_req(request):
-        try:
-            '''
-                Serialization and Deserialization of json 
-                data Strings
-                Tested in POSTMAN (raw body) with
-                {
-                    "data": "some text of some data"
-                }
-
-            '''
-            data = request.get_json()
-            data_serializedString = pickle.dumps( data )
-            data_deserialized = pickle.loads( data_serializedString )
-            for key in data_deserialized:
-                '''
-                    Save Deserialized json data Strings
-                    to Db 
-                '''
-                post = Api( "OK", "[]", data_deserialized[key] )
-                db.session.add(post)
-                db.session.commit()
-            '''
-                Return Output and/or Error 
-            '''               
-            return dict(data)
-
-        except Exception as e:
-                return dict(
-                    status='FAIL',
-                    message='Could not create that post.',
-                    error=str(e))
+			
+			data = request.args.get('data')
+			
+			meta_data = request.args.get('meta_data')
+			
+			status = request.args.get('status')
+			
+			post = Api( status, meta_data, data)
+			
+			db.session.add(post)
+			
+			db.session.commit()
+			
+			return dict( data, meta_data, status)
